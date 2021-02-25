f7c6dd4893f4e18deee0cab5a938c20e2685ef65
elsaalarcon <elsa032017@gmail.com>
2021-02-24 18:24:04 -0800

    Assignment done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,23 @@ class PagesController < ApplicationController
   end
 
   def increase
+    session[:number] = 1 if !session[:number]
+    session[:number] += 1
+    redirect_to '/'
   end
+
+  def subtract
+    session[:number] = 1 if !session[:number]
+    session[:number] -= 1
+    redirect_to '/'
+  end
+
+  def double
+    session[:number] = 1 if !session[:number]
+    ap session[:number] *= session[:number]
+    redirect_to '/'
+  end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#main'
   post 'increase', to: 'pages#increase'
+  post 'subtract', to: 'pages#subtract'
+  post 'double', to: 'pages#double'
 end
